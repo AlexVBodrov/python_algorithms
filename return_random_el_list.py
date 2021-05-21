d4d44937718b6d5a01ad2a22a1f2e04cdfca338e
AlexVBodrov <alexvbodrov@mail.ru>
2021-05-21 12:34:39 +0300

    add modules lesson # MODULES

--- return_random_el_list.py
+++ return_random_el_list.py
@@ -3,18 +3,15 @@
 Если список пустой функция должна вернуть None. Проверьте работу функций в этом же модуле.
     Примечание: Список для проверки введите вручную. Или возьмите этот: [1, 2, 3, 4]
 """
-import random
-from random import choice
 
+from random import choice
 
 def return_random_el(list):
     if len(list) == 0:
         return None
     else:
         # random.choice(sequence) - случайный элемент непустой последовательности
-        return random.choice(list)
-
-
+        return choice(list)
 
 if __name__ == '__main__':
     ls_qwerty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 11]
